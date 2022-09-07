1049d1e0f916ab98d8f191e1523348278c4d65cc
atwof <atwoff@gamil.com>
2022-09-07 19:29:00 -0300

    SellerService e FindAll

---
+++ Services/SellerService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ProjetoVendas.Models;
+
+namespace ProjetoVendas.Services
+{
+    public class SellerService
+    {
+        private readonly ProjetoVendasContext _context;
+
+        public SellerService(ProjetoVendasContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
+
--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using ProjetoVendas.Data;
+using ProjetoVendas.Services;
 
 namespace ProjetoVendas
 {
@@ -32,6 +33,7 @@ namespace ProjetoVendas
                                      builder => builder.MigrationsAssembly("ProjetoVendas")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

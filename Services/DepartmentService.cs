73ff5d8360aac3c14d51ba0306f338e559bd7de8
atwof <atwoff@gamil.com>
2022-09-13 20:16:39 -0300

    Operacoes assincronas

--- Services/DepartmentService.cs
+++ Services/DepartmentService.cs
@@ -2,6 +2,8 @@
 using ProjetoVendas.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace ProjetoVendas.Services
 {
@@ -14,9 +16,9 @@ namespace ProjetoVendas.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
